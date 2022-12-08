948550484a9206d62cf8aebd965de3f08c782fbd
Dave Walker <david.walker5@icloud.com>
2022-12-08 13:38:15 +0000

    Remove redundant tests

--- tests/odesolver/dyay.py
+++
@@ -1,9 +0,0 @@
-def f(_, y):
-    """
-    dy/dx = Ay
-
-    :param _: Independent variable (not used in this example)
-    :param y: Dependent variable
-    :return: Next value of the dependent variable
-    """
-    return 0.5 * y
