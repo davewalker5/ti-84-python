0e3594f4687da1230ada43be3ff1feb2fe9024f7
Dave Walker <david.walker5@icloud.com>
2022-12-18 12:48:54 +0000

    Use test mocks when running tests, coverage and building docs

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,22 @@
 
 export PROJECT_ROOT=$( cd "$(dirname "$0")" ; pwd -P )
 source "$PROJECT_ROOT/venv/bin/activate"
-export PYTHONPATH="$PROJECT_ROOT/src:$PROJECT_ROOT/src/common:$PROJECT_ROOT/src/mocks:$PROJECT_ROOT/src/maths"
+
+src_paths=""
+for dir in $PROJECT_ROOT/src/* ; do
+  dir_name=$dir:t
+  if [ -d "$dir" ] && [ "$dir_name" != "__pycache__" ] && [ "$dir_name" != "ti_desktop" ]; then
+    if [ "$src_paths" != "" ] ; then
+      src_paths="$src_paths:$dir"
+    else
+      src_paths=$dir
+    fi
+  fi
+done
+
+export PYTHONPATH="$PROJECT_ROOT/tests/mocks:$src_paths"
+
+echo Project root = $PROJECT_ROOT
+echo Python Path  = $PYTHONPATH
+
 python -m unittest
