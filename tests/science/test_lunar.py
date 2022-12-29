edfc93dfa42d0c2352b56aae42d449bc2b278d41
Dave Walker <david.walker5@icloud.com>
2022-12-29 13:00:32 +0000

    Reinstate unit tests

--- tests/science/test_lunar.py
+++ tests/science/test_lunar.py
@@ -20,24 +20,23 @@ class TestLunarCycle(unittest.TestCase):
     def test_lunar_cycle_age(self):
         number_of_days = 1 + int(self.LUNAR_CYCLE_LENGTH)
         cycle = calculate_lunar_cycle(self.REFERENCE_DATE, number_of_days)
-        print(cycle)
         for i in range(0, number_of_days + 1):
             expected = 0 if i == number_of_days else i
             self.assertEqual(expected, cycle[i]["age"])
-    #
-    # def test_lunar_cycle_phase(self):
-    #     number_of_days = 1 + int(self.LUNAR_CYCLE_LENGTH)
-    #     cycle = calculate_lunar_cycle(self.REFERENCE_DATE, number_of_days)
-    #     phase_name_index = 0
-    #     for i in range(0, number_of_days + 1):
-    #         if cycle[i]["phase"] != self.PHASE_NAMES[phase_name_index]:
-    #             phase_name_index = phase_name_index + 1
-    #             if phase_name_index >= 8:
-    #                 phase_name_index = 0
-    #
-    #         self.assertEqual(self.PHASE_NAMES[phase_name_index], cycle[i]["phase"])
-    #
-    # def test_out_of_range_date_raises_error(self):
-    #     with self.assertRaises(ValueError):
-    #         d = self.REFERENCE_DATE - 86400
-    #         _ = calculate_lunar_age(d)
+
+    def test_lunar_cycle_phase(self):
+        number_of_days = 1 + int(self.LUNAR_CYCLE_LENGTH)
+        cycle = calculate_lunar_cycle(self.REFERENCE_DATE, number_of_days)
+        phase_name_index = 0
+        for i in range(0, number_of_days + 1):
+            if cycle[i]["phase"] != self.PHASE_NAMES[phase_name_index]:
+                phase_name_index = phase_name_index + 1
+                if phase_name_index >= 8:
+                    phase_name_index = 0
+
+            self.assertEqual(self.PHASE_NAMES[phase_name_index], cycle[i]["phase"])
+
+    def test_out_of_range_date_raises_error(self):
+        with self.assertRaises(ValueError):
+            d = DateTime.from_timestamp(self.REFERENCE_DATE.timestamp() - 86400)
+            _ = calculate_lunar_age(d)
