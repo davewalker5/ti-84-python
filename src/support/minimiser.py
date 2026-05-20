30be999ddc14046905a8fa9987d3a33b78fa70b0
Dave Walker <david.walker5@icloud.com>
2026-05-20 10:59:00 +0100

    Remove additional (problematic) python_minify options

--- src/support/minimiser.py
+++ src/support/minimiser.py
@@ -137,14 +137,11 @@ def minify_file(
     source = "".join(lines)
     minified = minify(source,
                       file_path,
-                      remove_pass=True,
+                      remove_pass=False,
                       remove_literal_statements=True,
                       rename_locals=True,
                       rename_globals=aggressive,
-                      preserve_globals=preserve,
-                      remove_asserts=True,
-                      remove_debug=True,
-                      prefer_single_line=True)
+                      preserve_globals=preserve)
 
     # Write the "minimised" file
     output_file_path = output_folder / Path(file_path).name
